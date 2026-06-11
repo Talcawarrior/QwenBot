a45f70fe211d2d7bedca2de90e71c44b6f7357e1
Polymarket Bot Dev <dev@polymarket-bot.local>
2026-06-11 23:42:46 +0300

    chore: remove stale artifacts (logs,bat scripts,gitconfig,pngs,old test)

--- _start_bot_v2.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set PYTHONPATH=C:\Users\fdemir\Documents\New project\QwenBot
-set DRY_RUN=true
-set HOST=127.0.0.1
-set PORT=8091
-set FLAT_BET_USD=10
-set SCAN_INTERVAL=300
-cd /d C:\Users\fdemir\Documents\New project\QwenBot
-start /B python main.py run > logs\server.out.log 2> logs\server.err.log
